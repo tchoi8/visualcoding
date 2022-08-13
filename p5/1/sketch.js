2b87ac73e38389ddc37ef221091b4f838e611e14
Taeyooon Workshop <shootmegame@gmail.com>
2022-08-13 13:25:43 +0900

    updates

---
+++ p5/1/sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(500, 500);
+  background(220);  
+}
+
+function draw() {
+  
+  rectMode(CENTER);
+  rect(250,250,100,100);
+  triangle(200,300,300,300,250,200)
+  ellipse(250,250,100,100);
+  quad(200, 250, 300, 250, 270, 205, 230, 205);
+  triangle(220,220,280,220,250,250);
+  rectMode(CORNER);
+  rect(210,250,80,30) 
+  bezier(210,280, 220, 250, 280, 280, 290, 250);
+}
+
+
+
+
---
+++ p5/3/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(500, 500);
+  
+}
+
+function draw() {
+
+  background(100);
+  fill(200);
+  ellipse(250,250,160, 250)
+  fill(0);
+  triangle(250,125,280,100,220,100) 
+  circle(220,200,10,10)
+  circle(270,200,10,10)
+  ellipse(240,300,20,1)
+   
+}
